62df0d211683274f902214ba4f96c8315abedea4
qkyo1 <engawab8@gmail.com>
2024-03-28 03:54:24 +0400

    00

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - a functionn to ins3rt on left
+ * @parent: par3nt valu33
+ * @value: valu3 valu33
+ * Return: retu3n to nw3
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+}
